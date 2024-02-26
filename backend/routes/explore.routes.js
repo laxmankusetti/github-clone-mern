08bc75b4dbfcf67a02f5668cca451366049544ec
laxmankusetti <laxmankusetti@gmail.com>
2024-02-26 18:51:25 +0530

    debugged

--- backend/routes/explore.routes.js
+++ backend/routes/explore.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { exploreRepos } from '../controllers/explore.controller.js';
+import { explorePopularRepos } from '../controllers/explore.controller.js';
 import {ensureAuthenticated} from '../middlewares/ensureAuthenticated.js';
 
 const router = express.Router();
-router.get('/repos/:language',ensureAuthenticated, exploreRepos);
+router.get('/repos/:language',ensureAuthenticated, explorePopularRepos);
 
 export default router;
